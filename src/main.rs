1d555d33dfb723dfbb36375ab923ff6c746273ae
semadrig <semadrig@cisco.com>
2023-10-29 15:32:25 -0600

    Integrated websocket listener and sender for the bot embedded option.

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,6 @@
 // std
 use std::vec;
 
-// futures
-use futures_util::StreamExt;
-
 // log
 use log::{debug, info};
 
@@ -21,8 +18,8 @@ use service::service::WebSocketClient;
 use dotenv::dotenv;
 
 // Tokio
-use tokio::io::AsyncReadExt;
-use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
+use tokio::sync::mpsc::Receiver;
+use tokio_tungstenite::tungstenite::protocol::Message;
 
 // Rocket Dependencies
 
@@ -50,43 +47,11 @@ fn some_error(msg: &str) -> ! {
 // #########################################################################################
 
 // #########################################################################################
-// Intialize Websocket Client for the Embedded Communication.
-// #########################################################################################
-
-async fn init_websocket_client() {
-    // Register to the websocket server..
-    let client = WebSocketClient::new(
-        String::from(
-            std::env::var("WS_SERVER")
-                .expect("The TOKEN must be set.")
-                .as_str(),
-        ),
-        std::env::var("WS_SERVER_PORT")
-            .expect("The TOKEN must be set.")
-            .parse::<u16>()
-            .unwrap(),
-    );
-    let registration_url = client.register(6).await;
-    println!("Registration URL from server: {}", &registration_url.url);
-
-    // Parse the registration URL as of a URL type.
-    let url = url::Url::parse(&registration_url.url).unwrap();
-    debug!("Parsed registration string: {}", url);
-
-    // Retrieve the ws stream.
-    let (ws_stream, _) = connect_async(url).await.expect("Failed to connect");
-    info!("WebSocket handshake has been successfully completed");
-
-    // Split the full-duplex stream into sender and receiver.
-    let (mut ws_sender, mut ws_receiver) = ws_stream.split();
-}
-
-// #########################################################################################
 // Server Entrypoint.
 // #########################################################################################
 
-#[rocket::main]
-async fn main() -> Result<(), rocket::Error> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     ::std::env::set_var("RUST_LOG", "debug");
     env_logger::init();
 
@@ -133,8 +98,26 @@ async fn main() -> Result<(), rocket::Error> {
         .add_command(
             "/embedded",
             vec![Box::new(RequiredArgument::<String>::new("is_embedded"))],
-            move |client, message, _required_args, _optional_args| {
-                Box::pin(async move { debug!("Activated embedded version for the futbolito bot.") })
+            move |_client, _message, _required_args, _optional_args| {
+                Box::pin(async move {
+                    debug!("Activated embedded version for the futbolito bot.");
+                    // Setup the websocket client for communication with the embedded device to the webex bot.
+                    let client = WebSocketClient::new(
+                        "172.172.194.77",
+                        8080,
+                        2,
+                        vec![String::from("fut_assist")],
+                    );
+                    let registration_url = client.register().await;
+                    println!("Registration URL from server: {}", &registration_url.url);
+
+                    // Generate sender and receiver for the websocket crated.
+                    let (_sender, receiver) =
+                        client.start_ws_client(registration_url.url).await.unwrap();
+
+                    // Spawn a task to listen for incoming messages
+                    tokio::spawn(listen_for_messages(receiver));
+                })
             },
         )
         .await;
@@ -145,102 +128,16 @@ async fn main() -> Result<(), rocket::Error> {
     Ok(())
 }
 
-/*
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    ::std::env::set_var("RUST_LOG", "debug");
-    env_logger::init();
-
-    let client = WebSocketClient::new(String::from("172.172.194.77"), 8080);
-    let registration_url = client.register(6).await;
-    println!("Registration URL from server: {}", &registration_url.url);
-
-    // Parse the registration URL as of a URL type.
-    let url = url::Url::parse(&registration_url.url).unwrap();
-    debug!("Parsed registration string: {}", url);
-
-    let (stdin_tx, stdin_rx) = futures_channel::mpsc::unbounded();
-    tokio::spawn(read_stdin(stdin_tx));
-
-    let (ws_stream, _) = connect_async(url).await.expect("Failed to connect");
-    info!("WebSocket handshake has been successfully completed");
-
-    // Split the WebSocket into sender and receiver.
-    let (mut ws_sender, mut ws_receiver) = ws_stream.split();
-
-    // let stdin_to_ws = stdin_rx.map(Ok).forward(ws_sender);
-    // let ws_to_stdout = {
-    //     ws_receiver.for_each(|message| async {
-    //         let data = message.unwrap().into_data();
-    //         tokio::io::stdout().write_all(&data).await.unwrap();
-    //     })
-    // };
-
-    // -----------------------------------------------------------------
-    // stdin/stdout is used for testing purposes.
-    // -----------------------------------------------------------------
-    // Create a separate thread for sending messages.
-    let send_thread = thread::spawn(move || {
-        let stdin = io::stdin();
-        let mut stdout = io::stdout();
-        let mut input = String::new();
-
-        loop {
-            input.clear();
-            print!("Enter a message: ");
-            stdout.flush().unwrap();
-            stdin.read_line(&mut input).unwrap();
-
-            let trimmed = input.trim();
-            if trimmed.is_empty() {
-                continue;
-            }
-
-            let message = Message::Text(trimmed.to_string());
-
-            ws_sender.send(message);
-        }
-    });
-
-    // Handle incoming WebSocket messages
-    while let Some(Ok(msg)) = ws_receiver.next().await {
-        match msg {
+// Function to listen for incoming messages and process them
+async fn listen_for_messages(mut receiver: Receiver<Message>) {
+    while let Some(message) = receiver.recv().await {
+        match message {
             Message::Text(text) => {
-                println!("Received text message: {}", text);
-            }
-            Message::Binary(bin_data) => {
-                println!("Received binary message with {} bytes", bin_data.len());
-            }
-            Message::Close(_) => {
-                println!("WebSocket connection closed");
-                break;
+                println!("Received message: {}", text);
             }
             _ => {
-                // Handle other message types as needed
+                println!("Received non-text message");
             }
         }
     }
-
-    // Wait for the send thread to finish
-    send_thread.join().unwrap();
-
-    // pin_mut!(stdin_to_ws, ws_to_stdout);
-    // future::select(stdin_to_ws, ws_to_stdout).await;
-
-    Ok(())
-}*/
-
-// Our helper method which will read data from stdin and send it along the
-// sender provided.
-// async fn read_stdin(tx: futures_channel::mpsc::UnboundedSender<Message>) {
-//     let mut stdin = tokio::io::stdin();
-//     loop {
-//         let mut buf = vec![0; 1024];
-//         let n = match stdin.read(&mut buf).await {
-//             Err(_) | Ok(0) => break,
-//             Ok(n) => n,
-//         };
-//         buf.truncate(n);
-//         tx.unbounded_send(Message::binary(buf)).unwrap();
-//     }
-// }
+}
